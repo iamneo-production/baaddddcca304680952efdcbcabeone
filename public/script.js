2428026b5f5679af3be9be38c8b8e2f4d771e6bb
Iamneo <techadmin@iamneo.ai>
2023-09-03 18:30:23 +0000

    Backup Commit

--- public/script.js
+++ public/script.js
@@ -1,74 +1,61 @@
-const board = document.getElementById("board");
-const cells = document.querySelectorAll("[data-cell]");
-const message = document.getElementById("message");
-const restartButton = document.getElementById("restart");
+const PLAYER_X = 'X';
+const PLAYER_O = 'O';
+let currentPlayer = PLAYER_X;
+let isGameActive = true;
+const buttons = document.querySelectorAll('.btn');
+const resultContainer = document.querySelector('.result');
+const resetButton = document.getElementById('reset-btn');
+function handleButtonClick(event) {
+    const button = event.target;
+    if (button.textContent === '' && isGameActive) {
+        button.textContent = currentPlayer;
+        button.classList.add('filled');
+        if (checkWin() || checkDraw()) {
+            isGameActive = false;
+            resultContainer.textContent = isGameActive ? `Player ${currentPlayer}'s Turn` : 'Game Over';
+        } else {
+            currentPlayer = currentPlayer === PLAYER_X ? PLAYER_O : PLAYER_X;
+            resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
+        }
+    }
+}
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
 
-let currentPlayer = "X";
-let gameBoard = ["", "", "", "", "", "", "", "", ""];
-let gameActive = true;
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (buttons[a].textContent && buttons[a].textContent === buttons[b].textContent && buttons[a].textContent === buttons[c].textContent) {
+            resultContainer.textContent = `Player ${currentPlayer} Wins!`;
+            return true;
+        }
+    }
 
-const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-function handleCellClick(event) {
-  const cell = event.target;
-  const cellIndex = parseInt(cell.getAttribute("data-cell"));
-  if (gameBoard[cellIndex] === "" && gameActive) {
-    gameBoard[cellIndex] = currentPlayer;
-    cell.textContent = currentPlayer;
-    cell.style.backgroundColor = currentPlayer === "X" ? "lightcoral" : "lightblue";
-    togglePlayer();
-    checkWinner();
-  }
+    return false;
 }
 
-function togglePlayer() {
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
+function checkDraw() {
+    const isDraw = [...buttons].every(button => button.textContent !== '');
+    if (isDraw) {
+        resultContainer.textContent = 'It\'s a Draw!';
+        return true;
+    }
+    return false;
 }
-
-function checkWinner() {
-  for (const combination of winningCombinations) {
-    const [a, b, c] = combination;
-    if (
-      gameBoard[a] &&
-      gameBoard[a] === gameBoard[b] &&
-      gameBoard[a] === gameBoard[c]
-    ) {
-        gameActive = false;
-        message.textContent = `${currentPlayer} wins!`;
-        cells[a].style.backgroundColor = cells[b].style.backgroundColor = cells[
-          c
-        ].style.backgroundColor = "gold";
-        return;
-      }
-    }
-  
-    if (!gameBoard.includes("") && gameActive) {
-      gameActive = false;
-      message.textContent = "It's a draw!";
-    }
-  }
-  
-  function restartGame() {
-    gameBoard = ["", "", "", "", "", "", "", "", ""];
-    currentPlayer = "X";
-    gameActive = true;
-    message.textContent = "";
-    cells.forEach((cell) => {
-      cell.textContent = "";
-      cell.style.backgroundColor = "lightgray";
+function resetGame() {
+    buttons.forEach(button => {
+        button.textContent = '';
+        button.classList.remove('filled');
     });
-  }
-  cells.forEach((cell) => {
-    cell.addEventListener("click", handleCellClick);
-  });
-  
-  restartButton.addEventListener("click", restartGame);  
+
+    currentPlayer = PLAYER_X;
+    isGameActive = true;
+    resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
+}
+buttons.forEach(button => {
+    button.addEventListener('click', handleButtonClick);
+});
+resetButton.addEventListener('click', resetGame);
