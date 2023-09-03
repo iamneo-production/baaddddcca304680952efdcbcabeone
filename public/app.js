00ba9ab7ec33456518f1f84e141bf045ce33085d
Iamneo <techadmin@iamneo.ai>
2023-09-03 16:11:37 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,64 +1,74 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+const board = document.getElementById("board");
+const cells = document.querySelectorAll("[data-cell]");
+const message = document.getElementById("message");
+const restartButton = document.getElementById("restart");
+
+let currentPlayer = "X";
+let gameBoard = ["", "", "", "", "", "", "", "", ""];
+let gameActive = true;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Your game logic here
+function handleCellClick(event) {
+  const cell = event.target;
+  const cellIndex = parseInt(cell.getAttribute("data-cell"));
 
-    /*
-    **Part 1: Winning Conditions (Add your code here)**
-
-    1. Implement the logic to check for winning conditions using the 'conditions' array.
-    2. Display a winning message in the 'result' element when a player wins.
-    3. Disable all buttons after a win.
-    */
-
-    // Your code to update the game state and check for a win
-    // ...
-
-    // Your code to display the current player's turn
-    // ...
-
-    // Your code to handle button and cell interactions
-    // ...
-};
-
-    /*
-    **Part 2: Reset Function (Add your code here)**
-
-    1. Implement a new function that resets the game to its initial state.
-    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-    3. Update the 'result' element to indicate the current player's turn.
-    4. Re-enable all buttons for a new game.
-    */
-
-// Function to reset the game
-const resetGame = () => {
-    // Your code to reset the game state
-    // ...
-
-    // Your code to update the 'result' element
-    // ...
-
-    // Your code to re-enable buttons
-    // ...
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
-});
-
-document.querySelector('#reset').addEventListener('click', resetGame);
+  if (gameBoard[cellIndex] === "" && gameActive) {
+    gameBoard[cellIndex] = currentPlayer;
+    cell.textContent = currentPlayer;
+    cell.style.backgroundColor = currentPlayer === "X" ? "lightcoral" : "lightblue";
+    togglePlayer();
+    checkWinner();
+  }
+}
+function togglePlayer() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+  
+  function checkWinner() {
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (
+        gameBoard[a] &&
+        gameBoard[a] === gameBoard[b] &&
+        gameBoard[a] === gameBoard[c]
+      ) {
+        gameActive = false;
+        message.textContent = `${currentPlayer} wins!`;
+        cells[a].style.backgroundColor = cells[b].style.backgroundColor = cells[
+          c
+        ].style.backgroundColor = "gold";
+        return;
+      }
+    }
+  
+    if (!gameBoard.includes("") && gameActive) {
+      gameActive = false;
+      message.textContent = "It's a draw!";
+    }
+  }
+  function restartGame() {
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    currentPlayer = "X";
+    gameActive = true;
+    message.textContent = "";
+    cells.forEach((cell) => {
+      cell.textContent = "";
+      cell.style.backgroundColor = "lightgray";
+    });
+  }
+  
+  cells.forEach((cell) => {
+    cell.addEventListener("click", handleCellClick);
+  });
+  
+  restartButton.addEventListener("click", restartGame);  
